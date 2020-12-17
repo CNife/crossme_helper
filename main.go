8e446db6e7dc1f9bafb22a74cdd6c98e54e28910
CNife <CNife@vip.qq.com>
2020-12-17 18:44:18 +0800

    1.0.0

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(initText)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			panic(scanner.Err())
+		}
+		line := scanner.Text()
+
+		line = strings.ToLower(strings.TrimSpace(line))
+		words := strings.Split(line, " ")
+		if len(words) > 0 {
+			err := exec(words[0], words[1:])
+			if err == quitWithoutError {
+				return
+			}
+			if err != nil {
+				_, err = fmt.Fprintln(os.Stderr, err)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
+
+const initText = `交互式 CrossMe 游戏助手
+输入 help 获取帮助，输入 quit 退出
+输入命令时可以只输入前缀`
